4665e2533bac1a9ca6061c388db72a5e73a98558
rakafebriansy <kerenr445@gmail.com>
2025-02-05 19:19:15 +0700

    chore: setup project

---
+++ src/common/common.module.ts
@@ -0,0 +1,42 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+import { PrismaService } from './prisma/prisma.service';
+import { ValidationService } from './validation/validation.service';
+import * as winston from 'winston';
+
+@Global()
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        WinstonModule.forRoot({
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                nestWinstonModuleUtilities.format.nestLike()
+            ),
+            transports: [
+                new winston.transports.File({
+                    filename: 'logs/error.log',
+                    level: 'error',
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.json()
+                    )
+                }),
+                new winston.transports.File({
+                    filename: 'logs/combined.log',
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.json()
+                    )
+                }),
+            ]
+        })],
+    providers: [PrismaService, ValidationService],
+    exports: [PrismaService, ValidationService],
+})
+
+export class CommonModule { }
